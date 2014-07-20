8e7bc637efb668892c1e10b28193b04310da3d06
gustav.rodewald <gustav.rodewald@asurion.com>
2014-07-20 17:18:52 -0500

    refactored bomb factory so that it generates a random sequence of bombs each time

--- src/js/mine_sweeper_board/bombFactory.js
+++ src/js/mine_sweeper_board/bombFactory.js
@@ -5,20 +5,34 @@ define([], (function() {
 
     create = function(width, height, bombs) {
 
-        var generateBombArray;
+        var generateBombArray, shuffleArray;
 
         generateBombArray = function () {
-            var bombArray = new Array(bombs ? bombs : 0);
-            for ( var i = 0; i < bombs; i = i + 1 ) {
+            var bombArray = new Array(width * height);
+            for ( var i = 0; i < width * height; i = i + 1 ) {
                 bombArray[i] = i;
             }
             return bombArray;
         };
 
-        if (bombs > (width * height)) {
+        shuffleArray = function (array) {
+            var swapIndex, swapValue;
+
+            for (var i = array.length - 1; i > 0; i = i -1) {
+                swapIndex = Math.floor(Math.random() * i);
+                swapValue = array[swapIndex];
+                array[swapIndex] = array[i];
+                array[i] = swapValue;
+            }
+
+            return array;
+        };
+
+        if (!bombs || bombs > (width * height)) {
             return [];
         }
-        return generateBombArray();
+        return shuffleArray(generateBombArray(bombs)).slice(0, bombs)
+                .sort(function (a,b) { return a - b; });
     };
 
     return {

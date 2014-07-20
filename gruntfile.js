ee66078c1ce996b04b09d6328c6e4f875da821a7
gustav.rodewald <gustav.rodewald@asurion.com>
2014-07-20 10:29:17 -0500

    initial checkin: gurnt/bower/requirejs added

---
+++ gruntfile.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = function (grunt){
+    grunt.initConfig({
+        bower: {
+            install: {
+
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-bower-task');
+};

3ac8483d4b0ca45240cc83f02c529e0e487f1df4
gustav.rodewald <gustav.rodewald@asurion.com>
2014-07-23 06:12:25 -0500

    don't need routes

--- src/routes.js
+++
@@ -1,9 +0,0 @@
-/*global define */
-define(['angular', 'app'], function(angular, app) {
-    'use strict';
-    return app.config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/mineSweeperBoard', {
-            templateUrl: 'views/mineSweeperBoard.html'
-        });
-    }]);
-});
